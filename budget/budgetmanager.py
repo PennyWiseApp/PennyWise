345e5fc79389fbdf98607e5b1a49ce665c65368a
GeorgeWHill <97958357+GeorgeWHill@users.noreply.github.com>
2024-02-28 11:42:44 +0000

    Change to budget manager to add category class

--- budget/budgetmanager.py
+++ budget/budgetmanager.py
@@ -1,46 +1,60 @@
 # import matplotlib.pyplot as plt
 
+class budgetCategory:
+    def __init__(self, description, priority, limit):
+        self.description = description
+        self.priority = priority
+        self.limit = limit
+        self.spent = 0
+        self.can_notify = True
+    
+    def update_priority(self, new_priority):
+        self.priority = new_priority
+        
+    def check_limit(self):
+        if self.spent >= self.limit:
+            self.notify("limit")
+        elif self.spent >= 0.8 * self.limit:
+            self.notify("warning")
+            
+    def notify(self, type):
+        if self.can_notify:
+            if type == "limit":
+                print("ALERT: Spending limit reached or exceeded for " + self.description)
+            elif type == "warning":
+                print("WARNING: You are approaching the limit for " + self.description)
+            
 
 class BudgetManager:
     def __init__(self, overall_budget):
         self.overall_budget = overall_budget
-        self.categories = {}
-        self.notifications = {}
+        self.categories = []
 
     def generate_budget(self):
         print("Generating Budget...")
         # Logic for generating budget based on user inputs
 
-    def add_category_limit(self, category, limit):
-        self.categories[category] = {'limit': limit, 'spent': 0}
+    def add_category(self, category, priority, limit):
+        new_category = budgetCategory(category, priority, limit)
+        self.categories.append(new_category)
+        
+    def get_category(self, category_name):
+        for i in self.categories:
+            if i.description == category_name:
+                return i
 
     def update_spending(self, category, amount):
-        if category in self.categories:
-            self.categories[category]['spent'] += amount
-            self.check_category_limit(category)
-
-    def check_category_limit(self, category):
-        if category in self.categories:
-            limit = self.categories[category]['limit']
-            spent = self.categories[category]['spent']
-            if spent >= limit:
-                self.notify_alert(category)
-            elif spent >= 0.8 * limit:
-                self.notify_warning(category)
-
-    def notify_alert(self, category):
-        print(f"ALERT: You have reached the spending limit for {category}!")
-
-    def notify_warning(self, category):
-        print(f"WARNING: You are close to reaching the spending limit for {category}.")
+        budget_category = self.get_category(category)
+        budget_category.spent += amount
+        budget_category.check_limit()
 
     def disable_notification(self, category):
-        if category in self.notifications:
-            self.notifications[category] = False
+        budget_category = self.get_category(category)
+        budget_category.can_notify = False
 
     def enable_notification(self, category):
-        if category in self.notifications:
-            self.notifications[category] = True
+        budget_category = self.get_category(category)
+        budget_category.can_notify = True
 
     def make_purchase(self, amount, category):
         # Connect to bank details logic can be added here
@@ -57,8 +71,8 @@ budget_manager = BudgetManager(1000)  # Set overall budget to 1000
 
 budget_manager.generate_budget()
 
-budget_manager.add_category_limit('Groceries', 200)
-budget_manager.add_category_limit('Entertainment', 100)
+budget_manager.add_category('Groceries', 8, 200)
+budget_manager.add_category('Entertainment', 4, 100)
 
 budget_manager.make_purchase(50, 'Groceries')
 budget_manager.make_purchase(30, 'Entertainment')
@@ -66,6 +80,8 @@ budget_manager.make_purchase(30, 'Entertainment')
 budget_manager.disable_notification('Entertainment')
 
 budget_manager.make_purchase(80, 'Entertainment')
+
+print(budget_manager.categories)
 
 
 
